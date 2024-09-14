62088c2f8a8165c22c8059ee9c72d22e6fc76e98
Monu_2114 <monu.nanabala@gmail.com>
2024-09-14 07:50:34 +0530

    delay none

--- src/Image.jsx
+++ src/Image.jsx
@@ -3,7 +3,7 @@ export default function Image({ url }) {
     <img
       src={url}
       className="transition ease-out hover:-translate-y-4 hover:-rotate-2 hover:scale-110 w-[190px] h-[179px] rounded-[24px]
-   transition-opacity grayscale hover:grayscale-0 duration-500  delay-"
+   transition-opacity grayscale hover:grayscale-0 duration-500  "
       alt="dynamic"
     />
   );
