6ba64f36499d40c8524c7ce24357b39258ff9a39
Monu_2114 <monu.nanabala@gmail.com>
2024-09-14 07:33:02 +0530

    Images moved to public

--- src/Widget-1.jsx
+++ src/Widget-1.jsx
@@ -2,7 +2,7 @@ export default function Widget1() {
   return (
     <div className=" py-5 pl-3	flex flex-col gradient-shadow  bg-darkBlack w-[720px] h-[320px] rounded-2xl mr-10 gap-y-16 mt-10">
       <div className="flex gap-x-2">
-        <img src="src\assets\question.svg" className="w-8 h-6" alt="" />
+        <img src="images/question.svg" className="w-8 h-6" alt="" />
         <div className="flex flex-col">
           <div className="cursor-pointer flex gap-x-20 display bg-black h-[52px] w-[614px] rounded-3xl py-3 pl-20 text-textColor 	tracking-widest  font-poppins text-lg font-medium	 ">
             <div className="bg-background hover:text-white rounded-xl">
@@ -13,7 +13,7 @@ export default function Widget1() {
           </div>
           <div className="flex py-6 ">
             <img
-              src="src\assets\box.svg"
+              src="/images/box.svg"
               className="w-8 h-8 relative -translate-x-8 translate-y-8"
               alt=""
             />
@@ -33,7 +33,7 @@ export default function Widget1() {
             </div>
 
             <img
-              src="src\assets\Rectangle 39335.svg "
+              src="images/Rectangle 39335.svg "
               className="w-8 h-20 relative -translate-y-10  "
               alt=""
             />
--- src/Widget-2.jsx
+++ src/Widget-2.jsx
@@ -1,14 +1,14 @@
 import Image from "./Image";
 export default function Widget2() {
   let img_arr = [
-    "src/assets/image-1.jpeg",
-    "src/assets/image-2.jpeg",
-    "src/assets/image-3.jpeg",
+    "images/image-1.jpeg",
+    "images/image-2.jpeg",
+    "images/image-3.jpeg",
   ];
   return (
     <div className=" py-5 pl-3	flex flex-col gradient-shadow  bg-darkBlack w-[720px] h-[320px] rounded-2xl mr-10 gap-y-4">
       <div className="flex gap-x-2 items-top ">
-        <img src="src\assets\question.svg" className="w-8 h-6" alt="" />
+        <img src="/images/question.svg" className="w-8 h-6" alt="" />
         <div className="flex gap-x-28 items-start">
           <button className="text-base h-[50px] w-[120px] bg-black text-white font-bold py-2 px-6 border border-black rounded-xl font-poppins text-lg font-medium">
             Gallery
@@ -19,11 +19,11 @@ export default function Widget2() {
             </button>
             <div className="flex  gap-0">
               <img
-                src="src\assets\left.svg"
+                src="/images/left.svg"
                 alt=""
                 className="w-28  h-24	flex-shrink-0"
               />
-              <img src="src\assets\right.svg" alt="" className="w-28  h-24  " />
+              <img src="images/right.svg" alt="" className="w-28  h-24  " />
             </div>
           </div>
         </div>
@@ -33,7 +33,7 @@ export default function Widget2() {
 
 "
       >
-        <img src="public/assets/box.svg" className="w-8 h-8 " alt="" />
+        <img src="/images/box.svg" className="w-8 h-8 " alt="" />
         {img_arr.map((img, index) => (
           <Image key={index} url={img} alt="image" />
         ))}
