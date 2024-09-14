a8e9c82facbcc7018ba8d8de0dc85a76d232c754
Monu_2114 <monu.nanabala@gmail.com>
2024-09-14 07:45:48 +0530

    slow transition

--- src/Image.jsx
+++ src/Image.jsx
@@ -2,8 +2,8 @@ export default function Image({ url }) {
   return (
     <img
       src={url}
-      className="transition ease-in-out hover:-translate-y-4 hover:-rotate-2 hover:scale-110 w-[190px] h-[179px] rounded-[24px]
-   transition-opacity grayscale hover:grayscale-0 duration-300"
+      className="transition ease-out hover:-translate-y-4 hover:-rotate-2 hover:scale-110 w-[190px] h-[179px] rounded-[24px]
+   transition-opacity grayscale hover:grayscale-0 duration-500  delay-"
       alt="dynamic"
     />
   );
